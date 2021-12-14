be40f16300b16776322c31b1a054c578c4d5cc94
Gaabr47 <73695219+Gaabr47@users.noreply.github.com>
2021-12-14 07:02:16 -0300

    Add files via upload

--- src/home/index.js
+++ src/home/index.js
@@ -9,20 +9,28 @@ import { Link, useParams } from 'react-router-dom'
 import { PostContext } from '../context/index'
 export default function Home() {
     const {id} = useParams()
- /*    const { buscaOn, setBuscaOn, arrayNavigation } = useContext((PostContext)) */
+    const { buscaOn, setBuscaOn, arrayNavigation } = useContext(PostContext)
 
 
     return (
         <main style={{display:'flex',flexDirection:'column'}}>
           <div className="busca">
                 <form>
-                    <input  type="search" placeholder='Busque um filme por nome ou ano...' />
+                    <input onChange={(e) => setBuscaOn(e.target.value)} type="search" placeholder='Busque um filme por nome ou ano...' />
                 </form>
             </div>
            
             <div className="navigation">
                 <ul>
-                   
+                    {arrayNavigation.map((item,index) => {
+                        return (
+
+<div>   
+
+                            <li key={index}><Link to={`/page/${item}`}>{item === id ? 'Igual' : item }{id}</Link></li>
+                            </div>
+                        )
+                    })}
                 </ul>
             </div>
 
