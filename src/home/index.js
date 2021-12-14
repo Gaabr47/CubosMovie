f90564428072132b5318b3e7a95a840fd33d6881
Gaabr47 <73695219+Gaabr47@users.noreply.github.com>
2021-12-14 07:26:26 -0300

    Add files via upload

--- src/home/index.js
+++ src/home/index.js
@@ -4,35 +4,36 @@ import Postagens from '../components/posts'
 import './style.css'
 
 import Busca from '../components/busca'
-import { useState, useContext } from 'react';
+import { useContext } from 'react';
 import { Link, useParams } from 'react-router-dom'
 import { PostContext } from '../context/index'
 export default function Home() {
-    const {id} = useParams()
+    const { id } = useParams()
     const { buscaOn, setBuscaOn, arrayNavigation } = useContext(PostContext)
 
 
     return (
-        <main style={{display:'flex',flexDirection:'column'}}>
-          <div className="busca">
+        <main style={{ display: 'flex', flexDirection: 'column' }}>
+            <div className="busca">
                 <form>
                     <input onChange={(e) => setBuscaOn(e.target.value)} type="search" placeholder='Busque um filme por nome ou ano...' />
                 </form>
             </div>
-               {buscaOn === '' ?
+            {buscaOn === '' ?
                 
                 <Postagens />
                 :
                 <Busca />
             }
+
             <div className="navigation">
                 <ul>
-                    {arrayNavigation.map((item,index) => {
+                    {arrayNavigation.map((item, index) => {
                         return (
 
-<div>   
+                            <div>
 
-                            <li key={index}><Link to={`/page/${item}`}>{item === id ? 'Igual' : item }{id}</Link></li>
+                                <li key={index}><Link to={`/page/${item}`}>{item === id ? 'Igual' : item}{id}</Link></li>
                             </div>
                         )
                     })}
