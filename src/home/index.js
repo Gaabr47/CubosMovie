43c37b7207e6ce7baf24cba77091a318ffdd9a6e
Gaabr47 <73695219+Gaabr47@users.noreply.github.com>
2021-12-14 07:39:04 -0300

    Add files via upload

--- src/home/index.js
+++ src/home/index.js
@@ -2,14 +2,52 @@
 
 import Postagens from '../components/posts'
 import './style.css'
-
+import api from '../api/api';
 import Busca from '../components/busca'
-import { useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { Link, useParams } from 'react-router-dom'
 import { PostContext } from '../context/index'
 export default function Home() {
     const { id } = useParams()
     const { buscaOn, setBuscaOn, arrayNavigation } = useContext(PostContext)
+    const [postList, setPostList] = useState([])
+    const [generos, setGeneros] = useState([])
+    const [paginacao, setPaginacao] = useState('')
+    const { setNavigation } = useContext(PostContext)
+
+
+
+    useEffect(() => {
+        async function listMovies() {
+            const response = {
+                principal: await api.get('/popular?api_key=fe23e2883b46907fcadb189b39420b26&language=pt-br&page=4'),
+                generos: await api.get('https://api.themoviedb.org/3/genre/movie/list?api_key=fe23e2883b46907fcadb189b39420b26&language=pt-br')
+            }
+
+            setGeneros(response.generos.data.genres)
+
+            setPostList(response.principal.data.results)
+
+
+
+        }
+
+        if (id === undefined || id === '1') {
+            setPaginacao('0')
+
+        } else {
+            setPaginacao(id * 5)
+        }
+
+        listMovies()
+
+
+    }, [id])
+
+
+
+    setNavigation(postList.length)
+
 
 
     return (
@@ -20,8 +58,60 @@ export default function Home() {
                 </form>
             </div>
             {buscaOn === '' ?
-                
-               '123'
+                <article>
+                    { 
+                postList.slice(paginacao, paginacao + 5).map((item, index) => {
+                    const percentPosts = item.vote_average * 10
+
+                    return (
+                    <section key={index}>
+
+                        <div className="content_post">
+                            <div className="titulo"> <span className="porcentagem" style={{ padding: 20, borderRadius: '70%' }}>{percentPosts}%</span><h1><a href={`/${item.id}`}>{item.title}</a></h1>
+
+                            </div>
+
+                            <div className="center_content_post">
+                                <div style={{ marginLeft: '15%' }}> {item.release_date}</div>
+                                <br />
+
+                                {item.overview}
+
+
+                                {item.genre_ids === undefined ? console.log('undefined') :
+                                    <div className="categoria">
+                                        {
+                                            item.genre_ids.map((ids, index) => {
+
+                                                return (
+                                                    <ul key={index}>
+
+                                                        {generos.map((items, index) => {
+                                                            return (
+                                                                <div key={index}>
+                                                                    {items.id === ids ? <li>{items.name}</li> : <div style={{ display: 'none' }}></div>}
+                                                                </div>
+                                                            )
+                                                        })}
+                                                    </ul>
+                                                )
+
+                                            })
+                                        }</div>
+                                }
+
+                            </div>
+                        </div>
+                        <span>
+                            <img alt="poster" src={`https://image.tmdb.org/t/p/original/${item.poster_path}`} />
+                        </span>
+                    </section>
+                    )
+
+
+                })
+            }
+                </article>
                 :
                 <Busca />
             }
